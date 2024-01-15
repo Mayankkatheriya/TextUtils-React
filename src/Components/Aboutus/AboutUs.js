0aba227bbf771eb7abcf3239889fb5fafef2ad9a
Mayankkatheriya <mayankkatheriya4@gmail.com>
2024-01-16 01:40:56 +0530

    final commit

--- src/Components/Aboutus/AboutUs.js
+++ src/Components/Aboutus/AboutUs.js
@@ -1,14 +1,39 @@
-import './AboutUs.css'
-import React from 'react'
+import React, { useState } from "react";
+import "./AboutUs.css";
+import Data from "./Data";
 
-const AboutUs = () => {
+function AboutUs() {
+  const [currentData, setCurrentData] = useState(Data);
+
+  const expandArea = (idx) => {
+    setCurrentData((prevData) => {
+      return prevData.map((item, i) => {
+        if (i === idx) {
+          return { ...item, expanded: !item.expanded };
+        }
+        return { ...item, expanded: false };
+      });
+    });
+  };
+
   return (
     <main>
-        <section className="about">
-            About
-        </section>
+      <section className="about">
+        {currentData.map((item, idx) => (
+          <div
+            key={idx}
+            className={`sector ${item.expanded ? "expanded" : ""}`}
+          >
+            <div className="heading" onClick={() => expandArea(idx)}>
+              <h3>{item.heading}</h3>
+              <button>{(item.expanded) ? "➖" : "➕"}</button>
+            </div>
+            <p>{item.content}</p>
+          </div>
+        ))}
+      </section>
     </main>
-  )
+  );
 }
 
-export default AboutUs
+export default AboutUs;
---
+++ src/Components/Aboutus/Data.js
@@ -0,0 +1,19 @@
+const Data = [
+  {
+    heading: "Analyse your text",
+    content:
+      "Textutils gives you a way to analyze your text quickly and efficently.It let you to count word, count charecters or reading time required.It has both light and dark mode for better compartable.",
+  },
+  {
+    heading: "Free to use",
+    content:
+      "TextUtils is a free charecter counter tool that provided instant charecter count and word count statics for a given text. TextUtils reports the number of words and charecter. Thus it is suitable for writing text with word / charecter limit.",
+  },
+  {
+    heading: "Browser Compatible",
+    content:
+      "This word counter software works in any web browser such as Chrome , Firefox ,Internet Explorer ,Safari,Opera etc.",
+  },
+];
+
+export default Data;
